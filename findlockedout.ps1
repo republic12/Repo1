91723b3841e6287ffbbc8077ac3a51a9bac1eb95
skip hofmann <shofmann@republicservices.com>
2019-07-25 14:48:11 -0700

    initial commit

---
+++ findlockedout.ps1
@@ -0,0 +1,36 @@
+$serverList = @();
+Get-ADComputer -LDAPFilter '(cn=*DC*)' | % { $serverList += $_.DNSHostName; }
+Invoke-Command -ComputerName $serverList -ScriptBlock {
+$select = @{
+    'Username' = 5
+    'LogonType' = 10
+    'FailureReason' = 8
+    'LogonProcessName' = 11
+    'AuthenticationPackageName' = 12
+    'WorkstationName' = 13
+    'TargetDomainName' = 6
+    #'ProcessId' = 17
+    'ProcessName' = 18
+    'IpAddress' = 19
+
+
+}
+
+$events = get-winevent -FilterHashtable @{'LogName'= 'Security';'Id' = 4625} -MaxEvents 30
+
+$selected_events = ForEach ($event in $events)
+{
+    $new_event = $event
+    ForEach($key in $select.Keys)
+    {
+        $new_event = $new_event | 
+        Select-Object *,@{
+            'Name' = $key
+            'Expression' = { $_.Properties[$select[$key]].Value }
+        }
+    }
+    $new_event
+}
+
+$selected_events | 
+Select-Object -Property ([array]'TimeCreated' + [String[]]$select.Keys)} >c:\temp\lockout.log
